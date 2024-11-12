69b42f28ca623460c99e37c8fbbc36ec37e0b908
Lhostme <117309982+Lhostme@users.noreply.github.com>
2024-11-12 21:50:07 +0200

    Add build command

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # Remote Image Processing Service?
 
 ## Build
+docker build -t my-python-app .
+
+## Run
 docker run -it --rm -v $(pwd)/data:/data my-python-app
--- your_script.py
+++ your_script.py
@@ -5,7 +5,7 @@ def main():
     print("Hello World!")
     time.sleep(5)
     print("done")
-    time.sleep(3)
+    time.sleep(10)
 
 if __name__ == "__main__":
     main()
