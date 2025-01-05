751252667c8d0d6ef2caa9ed8ce7d544f2fa5b04
Lhostme <117309982+Lhostme@users.noreply.github.com>
2025-01-05 19:51:25 +0200

    Implement request parameters.

--- your_script.py
+++ your_script.py
@@ -9,6 +9,8 @@ app = Flask(__name__)
 @app.route('/sample', methods=['POST'])
 def sample():
     print(os.listdir())
+    reqArgs = request.get_json()
+    print("args: ", reqArgs)
 
     files = os.listdir("/")
     volume = "/" + next((s for s in files if "vidData" in s), None)
@@ -27,6 +29,8 @@ def sample():
     # Get video properties
     frameWidth = int(capture.get(cv2.CAP_PROP_FRAME_WIDTH))
     frameHeight = int(capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    print("width: ", frameWidth)
+    print("height: ", frameHeight)
     fps = int(capture.get(cv2.CAP_PROP_FPS))
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 
@@ -40,14 +44,9 @@ def sample():
         if not ret:
             break
 
-        # Add text to the frame
-        text = "OpenCV Text Example"
-        position = (50, 50)  # Coordinates (x, y) for the text
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        font_scale = 1
-        color = (0, 255, 0)  # Green
-        thickness = 2
-        cv2.putText(frame, text, position, font, font_scale, color, thickness)
+        for arg in reqArgs:
+            if (arg == "topText" or arg == "bottomText" or arg == "centerText"):
+                putText(reqArgs[arg], frame, frameWidth, frameHeight, arg)
 
         # Write the frame to the output video
         output.write(frame)
@@ -60,6 +59,25 @@ def sample():
     print(os.listdir(volume))
 
     return jsonify({"result":outputPath})
+
+def putText(text, frame, frameWidth, frameHeight, position):
+    # Add text to the frame
+    if not text:
+        text = "OpenCV Text Example"
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    font_scale = 1
+    color = (0, 255, 0)  # Green
+    thickness = 1
+    (text_width, text_height), baseline = cv2.getTextSize(text, font, font_scale, thickness)
+    xVal = round(frameWidth / 2)
+    if position == "topText":
+        position = (xVal - round(text_width / 2), text_height + 10)  # Coordinates (x, y) for the text
+    elif position == "bottomText":
+        position = (xVal - round(text_width / 2), frameHeight - text_height - 10)
+    else:
+        yVal = round(frameHeight / 2)
+        position = (xVal - round(text_width / 2), yVal) # Center text
+    cv2.putText(frame, text, position, font, font_scale, color, thickness)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
